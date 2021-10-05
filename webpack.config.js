b4057b0f57d69e12004f6bf11522561378cd5010
JakeClark33 <jakemclark@hotmail.com>
2021-10-04 23:36:07 -0500

    popper

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const bootstrap = require('bootstrap');
 const path = require('path');
 const webpack = require("webpack");
+import { createPopper } from '@popperjs/core';
+
+createPoppper() { createPopper(this.btnDropdownRef.nativeElement, this.popper, { placement: "bottom-end", }); this.btnDropdownRef.nativeElement.parentNode.insertBefore( this.popper, this.btnDropdownRef.nativeElement.nextSibling ); }
 
 module.exports = {entry: './assets/js/script.js',output: {
     path: path.resolve(__dirname, 'dist'),
