a46a0600bed6a1c195a27f8d3bb1f8e4bba03345
JakeClark33 <jakemclark@hotmail.com>
2021-10-05 17:04:54 -0500

    problem

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require("webpack");
 import { createPopper } from '@popperjs/core';
 
-createPoppper() { createPopper(this.btnDropdownRef.nativeElement, this.popper, { placement: "bottom-end", }); this.btnDropdownRef.nativeElement.parentNode.insertBefore( this.popper, this.btnDropdownRef.nativeElement.nextSibling ); }
+createPoppper(); { createPopper(this.btnDropdownRef.nativeElement, this.popper, { placement: "bottom-end", }); this.btnDropdownRef.nativeElement.parentNode.insertBefore( this.popper, this.btnDropdownRef.nativeElement.nextSibling ); }
 
 module.exports = {entry: './assets/js/script.js',output: {
     path: path.resolve(__dirname, 'dist'),
