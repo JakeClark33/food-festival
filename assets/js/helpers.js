2c6728cfe817a9353fb8e473f6ccf405d250f415
JakeClark33 <jakemclark@hotmail.com>
2021-10-05 19:21:12 -0500

    separation of concerns

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,34 +1,11 @@
-$(document).ready(function() {
-  function createEl(htmlString, attrs, ...children) {
-    if (typeof htmlString !== "string") {
-      throw Error("Argument 'htmlString' is required and must be a string");
-    }
-  
-    const el = document.createElement(htmlString);
-  
-    if (typeof attrs === "object") {
-      for (let key in attrs) {
-        if (key.substring(0, 2) === "on") {
-          el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-        } else {
-          el.setAttribute(key, attrs[key]);
-        }
-      }
-    }
-  
-    children.forEach(function(child) {
-      let node;
-  
-      if (child.constructor.name.includes("Element")) {
-        node = child;
-      } else {
-        node = document.createTextNode(child);
-      }
-  
-      el.appendChild(node);
-    });
-  
-    return el;
+dateConverter = function(UNIX_timestamp) {
+    const a = new Date(UNIX_timestamp);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const result =  month + ' ' + date + ', ' + year 
+    return result;
   };
 
   function createLoremIpsum(numWords = 50) {
@@ -39,46 +16,7 @@ $(document).ready(function() {
   };
 
 
-
- 
-
-  
-  // First image is hard coded in index.html
-  const carouselSlides = [
-    {
-      title: "We travel all over the US",
-      subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
-      btnText: "View Schedule",
-      btnUrl: "schedule.html"
-    },
-    {
-      title: "Our food is seriously the bomb!",
-      subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
-      btnText: "Purchase Tickets",
-      btnUrl: "tickets.html"
-    },
-  ]
-
-  carouselSlides.forEach((slide, i) => {
-    $('.carousel-inner').append(`
-  <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
-    <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-              <h2 class="display-4 mb-2">${slide.title}</h2>
-          </div>
-          <div class="row align-items-center justify-content-center"> 
-            <h3>${slide.subtitle}</h3>
-          </div>
-          <div class=" mt-4 row align-items-center justify-content-center"> 
-            <a class="btn btn-primary" href="${slide.btnUrl}">
-                ${slide.btnText}
-            </a>
-          </div>
-        </div>
-    </div>
-  </div>`)
-  })
-});
+  module.exports = {
+    dateConverter,
+    createLoremIpsum
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require("webpack");
 
@@ -8,6 +9,10 @@ module.exports = {entry: './assets/js/script.js',output: {
   plugins: [new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
-  }),],
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  })
+],
   mode: 'development'
 };
