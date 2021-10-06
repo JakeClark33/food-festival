3040c29f9e979edb7e97b13c9c9840f4af0815ea
JakeClark33 <jakemclark@hotmail.com>
2021-10-06 16:21:59 -0500

    4

--- service-worker.js
+++ service-worker.js
@@ -12,8 +12,7 @@ const FILES_TO_CACHE = [
     "./dist/app.bundle.js",
     "./dist/events.bundle.js",
     "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js",
-    "./dist/assets/icons"
+    "./dist/schedule.bundle.js"
   ];
   self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -43,19 +42,18 @@ const FILES_TO_CACHE = [
     );
   });
   self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+    })
+  )
   
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
   })
