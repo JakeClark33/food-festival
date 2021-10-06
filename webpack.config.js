22cdb2954aa978ac451280d36fb8922e96390232
JakeClark33 <jakemclark@hotmail.com>
2021-10-05 21:32:40 -0500

    loader and image pack webloader

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,36 @@ module.exports = {entry: {
     schedule: "./assets/js/schedule.js",
     tickets: "./assets/js/tickets.js"
   },
-    output: {
+  
+  output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname + '/dist'),
+    path: __dirname + "/dist"
   },
-    plugins: [new webpack.ProvidePlugin({
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+            {
+              loader: "file-loader",
+              options: {
+                esModule: false,
+                name (file) {
+                  return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                  return url.replace("../", "/assets/")
+                }
+              }  
+            },
+            {
+                loader: 'image-webpack-loader'
+              }
+          ]
+      }
+    ]
+  },
+      plugins: [new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery"
   }),
@@ -20,5 +45,5 @@ module.exports = {entry: {
     analyzerMode: "static", // the report outputs to an HTML file in the dist folder
   })
 ],
-  mode: 'development'
+mode: 'development'
 };
